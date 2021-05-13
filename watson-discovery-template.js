b99f14b0e5dc1f0db4abb2a96e5cf3189eec239a
Bryce Wilkinson <bjw123@users.noreply.github.com>
2021-05-13 20:51:52 +0900

    Create watson-discovery-template.js
    didnt transfer in pr

---
+++ watson-discovery-template.js
@@ -0,0 +1,34 @@
+const DiscoveryV1 = require('ibm-watson/discovery/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+
+const discovery = new DiscoveryV1({
+    version: '2019-04-30',
+    authenticator: new IamAuthenticator({
+        apikey: 'WqipI59T3pPZlTshaCoalrdNcrcp0k8vKuAaJcZSdOdp'
+    }),
+    serviceUrl: 'https://api.us-south.discovery.watson.cloud.ibm.com/instances/20965335-5ee4-4058-b3c0-ef976fc70e09',
+
+});
+const queryParams = {
+    environmentId: '{system}',
+    collectionId: '{news-en}',
+    //query: 'host::abc.net.au text:Quokka'
+    url: 'https://api.us-south.discovery.watson.cloud.ibm.com/instances/20965335-5ee4-4058-b3c0-ef976fc70e09/v1/environments/system/collections/news-en/query?version=2018-12-03&filter=text%3A%22Quokka%22&deduplicate=false&highlight=true&passages=true&passages.count=5&query=host%3A%3Aabc.net.au'
+
+};
+
+
+
+async function main(params) {
+    console.log("running")
+    discovery.query(queryParams)
+        .then(queryResponse => {
+            console.log("success:  ")
+            return(JSON.stringify(queryResponse));
+        })
+        .catch(err => {
+            console.log("error")
+            return(err);
+        });
+}
